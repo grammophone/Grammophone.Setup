254124ff9ef1ac93d6019bf7199e4a14f9460f16
grammophone <emmanuel.liossis@gmail.com>
2019-04-24 22:28:00 +0300

    Add ObjectFactory and CollectionFactory.

---
+++ CollectionFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Setup
+{
+	/// <summary>
+	/// Factory  to obtain collections of instances of type <typeparamref name="T"/>
+	/// from a dependencies container.
+	/// </summary>
+	/// <typeparam name="T">The type of object to obtain.</typeparam>
+	public class CollectionFactory<T>
+	{
+		#region Private fields
+
+		private readonly Settings settings;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="settings">The dependencies container.</param>
+		public CollectionFactory(Settings settings)
+		{
+			if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+			this.settings = settings;
+		}
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Obtain a collection of objects of <typeparamref name="T"/> as defined in the
+		/// dependencies container.
+		/// </summary>
+		public IEnumerable<T> Get() => settings.ResolveAll<T>();
+
+		/// <summary>
+		/// Obtain a dictionary of objects of <typeparamref name="T"/> as defined in the
+		/// dependencies container, where the key is the name under which they are defined.
+		/// </summary>
+		public IReadOnlyDictionary<string, T> GetToDictionary() => settings.ResolveAllToDictionary<T>();
+
+		/// <summary>
+		/// Determine whether named definitions for objects of type <typeparamref name="T"/>
+		/// exist in the dependencies container.
+		/// </summary>
+		public bool IsDefined() => settings.GetRegistrationNames<T>().Where(n => n != null).Any();
+
+		#endregion
+	}
+}
---
+++ ObjectFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace Grammophone.Setup
+{
+	/// <summary>
+	/// Factory to obtain objects of type <typeparamref name="T"/>
+	/// from a dependencies container.
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the collection to obtain.</typeparam>
+	public class ObjectFactory<T>
+	{
+		#region Private fields
+
+		private readonly Settings settings;
+
+		private readonly string name;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="settings">The dependencies container.</param>
+		[InjectionConstructor]
+		public ObjectFactory(Settings settings)
+			: this(settings, null)
+		{
+		}
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="settings">The dependencies container.</param>
+		/// <param name="name">The name of the configuration definition for the object of type <typeparamref name="T"/> or null for unnamed or implied definition.</param>
+		public ObjectFactory(Settings settings, string name)
+		{
+			if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+			this.settings = settings;
+			this.name = name;
+		}
+
+		#endregion
+
+		#region Public merthods
+
+		/// <summary>
+		/// Obtain an object of type <typeparamref name="T"/> as defined or implied from the
+		/// dependencies container.
+		/// </summary>
+		public T Get() => 
+			name != null ? settings.Resolve<T>(name) : settings.Resolve<T>();
+
+		/// <summary>
+		/// Determine whether explicit definition for objects of type <typeparamref name="T"/>
+		/// exists in the dependencies container.
+		/// </summary>
+		public bool IsDefined() =>
+			name != null ? settings.IsRegistered<T>(name) : settings.IsRegistered<T>();
+
+		#endregion
+	}
+}
